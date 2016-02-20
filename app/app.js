5c52c9826e8ae0769ed34b2aef1fcc5d3fd423fb
Phat Wangrungarun <phatograph@gmail.com>
2016-02-20 15:17:15 +0000

    Markers

--- app/app.js
+++ app/app.js
@@ -11,10 +11,88 @@ import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 const App = React.createClass({
   getInitialState() {
     return {
+      markers: [
+        {
+          position: {
+            lat: 56.3412378,
+            lng: -2.7953315,
+          },
+          key: "bus",
+          defaultAnimation: 2,
+          icon: 'bus.png'
+        },
+        {
+          position: {
+            lat: 56.3341776,
+            lng: -2.783473,
+          },
+          key: "albany",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 56.3415514 - 0.00022,
+            lng: -2.7951661 - 0.00054,
+          },
+          key: "library",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 56.3283371 - 0,
+            lng: -2.805844 - 0.0027,
+          },
+          key: "morrison",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 56.3355399 + 0.0002,
+            lng: -2.8208376 - 0.0007,
+          },
+          key: "dra",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 56.3404036 + 0.0004,
+            lng: -2.8101418 - 0.0037,
+          },
+          key: "agnes",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 56.3402233 + 0.00005,
+            lng: -2.7993857 - 0.0007,
+          },
+          key: "union",
+          defaultAnimation: 2
+        }
+      ]
     };
   },
 
   componentDidMount() {
+    $.get('http://138.251.207.124:4000/api/bus', () => {
+      console.log(arguments);
+    })
+    // setInterval(() => {
+    //   this.state.markers[0].position.lng -= 0.0005
+    //   this.setState({ markers: this.state.markers });
+    // }, 1000);
+  },
+
+  _gmapsElement() {
+    return (
+      <GoogleMap defaultZoom={14} defaultCenter={{lat: 56.3360378, lng: -2.8023315}}>
+        {this.state.markers.map((marker, index) => {
+          return (
+            <Marker {...marker} />
+          );
+        })}
+      </GoogleMap>
+    );
   },
 
   render() {
@@ -22,16 +100,8 @@ const App = React.createClass({
       <div>
         <h1>St Night Bus</h1>
         <div className={style.mapWrapper}>
-          <GoogleMapLoader
-            containerElement={<div className={style.map} />}
-            googleMapElement={
-              <GoogleMap
-                defaultZoom={14}
-                defaultCenter={{lat: 56.3412378, lng: -2.7953315}}
-              />
-            }
-          />
-        </div>
+          <GoogleMapLoader containerElement={<div className={style.map} />} googleMapElement={this._gmapsElement()} />
+          </div>
       </div>
     );
   }
